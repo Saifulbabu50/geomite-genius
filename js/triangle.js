f765f20f59be8d39c89820d1db5c544226308905
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-10-28 21:31:13 +0600

    triangle area

--- js/triangle.js
+++ js/triangle.js
@@ -11,6 +11,78 @@ function calculateTriangleArea() {
 
     const triangleAreaSpan = document.getElementById('triangle-area');
     triangleAreaSpan.innerText = area
+}
 
+// Rectangle area
+function calculateRectangleArea() {
+    const rectangleWinput = document.getElementById('Rectangle-w');
+    const rectangleWText = rectangleWinput.value
+    const white = parseFloat(rectangleWText)
+
+    const rectangleLinput = document.getElementById('Rectangle-l');
+    const rectangleLText = rectangleLinput.value
+    const longe = parseFloat(rectangleLText)
+
+    const area = white * longe
+    const rectangleAreaSpan = document.getElementById('Rectangle-area');
+    rectangleAreaSpan.innerText = area
+}
+
+// Parallelogram area
+function calculateParallelogramArea() {
+    const parallelogramBascinput = document.getElementById('parallelogram-basc')
+    const parallelogramBascText = parallelogramBascinput.value
+    const basc = parseFloat(parallelogramBascText)
+    console.log(basc)
+
+    const parallelogramHeightinput = document.getElementById('parallelogram-height')
+    const parallelogramHeightText = parallelogramHeightinput.value
+    const height = parseFloat(parallelogramHeightText)
+    console.log(height)
+
+    const area = basc * height
+
+    const parallelogramAreaSpan = document.getElementById('parallelogram-Area')
+    parallelogramAreaSpan.innerText = area
+
+}
+
+// Rhombus area
+
+function calculateRhombusArea() {
+    const rhombusinput = document.getElementById('rhombus-d1')
+    const rhombusText = rhombusinput.value
+    const d1 = parseFloat(rhombusText)
+    console.log(d1)
+
+    const rhombusAreainput = document.getElementById('rhombus-d2')
+    const rhombusAreaText = rhombusAreainput.value
+    const d2 = parseFloat(rhombusAreaText)
+    const d2x = d2 * 2
+    console.log(d2x)
+
+    const aera = 0.5 * d1 * d2x
+    const rhombusAreaSpan = document.getElementById('rhombus-Area');
+    rhombusAreaSpan.innerText = aera
+}
+
+// Pentagon area
+
+function calculatePentagonArea() {
+    const pentagonInput = document.getElementById('pentagon-b')
+    const pentagonText = pentagonInput.value
+    const pentagonB = parseFloat(pentagonText)
+    console.log(pentagonB)
+
+    const pentagonAreaInput = document.getElementById('pentagon-p')
+    const pentagonAreaText = pentagonAreaInput.value
+    const pentagonP = parseFloat(pentagonAreaText)
+    console.log(pentagonP)
+
+    const Area = 0.5 * pentagonB * pentagonP;
+    const pentagonAreaSpan = document.getElementById('pentagon-Area');
+    pentagonAreaSpan.innerText = Area
+
+    console.log(Area)
 
 }
